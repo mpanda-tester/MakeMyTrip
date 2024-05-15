b2d398af90b3ea2dbe4b0b0af87f4472563d6376
Crio.GitLab <admin@example.com>
2024-05-15 09:51:13 +0000

    Update TestCases.java

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -35,7 +35,7 @@ public class TestCases {
 
         // Set browser to maximize and wait
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 
     }
 
